9eae035aa1de6124eabfdad0df2e27506899c1db
AkdM <AkdM@users.noreply.github.com>
2026-09-21 10:53:05 +0200

    Site build 2026-09-21

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-document.head.appendChild(Object.assign(document.createElement('link'),{rel:'stylesheet',href:"/assets/index-CJ4NduGv.css"}));import("/assets/index-CGaOVDTp.js");
+document.head.appendChild(Object.assign(document.createElement('link'),{rel:'stylesheet',href:"/assets/index-CJ4NduGv.css"}));import("/assets/index-D6OGfvh2.js");
